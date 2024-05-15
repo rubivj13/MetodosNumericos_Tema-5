9988bc83bf81ca75b12fb34fbc17779c95a2d772
Rub_1 <Rub_1@192.168.9.17>
2024-05-14 20:50:10 -0500

    Subir Ejercicios 4

---
+++ src/main/java/Interpolación_de_lagrange/Ejercicio2.java
@@ -0,0 +1,29 @@
+
+package Interpolación_de_lagrange;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    
+ public static void main(String[] args) {
+        double[] x = {1, 2, 3, 4, 5};
+        double[] y = {2, 3, 5, 7, 11};
+        double targetX = 2.5;
+        double interpolatedY = interpolate(x, y, targetX);
+        System.out.println("Para x=" + targetX + ", y=" + interpolatedY);
+    }
+
+    public static double interpolate(double[] x, double[] y, double targetX) {
+        double result = 0;
+        for (int i = 0; i < x.length; i++) {
+            double term = y[i];
+            for (int j = 0; j < x.length; j++) {
+                if (j != i) {
+                    term *= (targetX - x[j]) / (x[i] - x[j]);
+                }
+            }
+            result += term;
+        }
+        return result;
+    }
+}
